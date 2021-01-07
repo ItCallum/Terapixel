f03327c7cfa3cdc60e163b8ef1eaf8c5c37bcb82
Callum Simpson <36512996+ItCallum@users.noreply.github.com>
2021-01-07 19:09:45 +0000

    Add files via upload
    This is the start of my 2nd version of this project. With Code based off the last attempt but cleaned and organised better. Also, this project is being re made due to a previous issue with git hub

---
+++ start.r
@@ -0,0 +1,11 @@
+getwd()
+
+#library("ProjectTemplate")
+#create.project("Terapixel_2")
+
+setwd("D:/Masters/Cloud/Terapixel/Terapixel_v2/Terapixel_2")
+load.project()
+
+library('ProjectTemplate')
+load.project()
+
