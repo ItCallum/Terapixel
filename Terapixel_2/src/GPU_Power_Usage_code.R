7601b514b61f842609e39fd281e634f538c3b8d0
Callum Simpson <36512996+ItCallum@users.noreply.github.com>
2021-01-19 23:17:16 +0000

    Add files via upload
    Updated and cleaned the code
    
    Going to start investigating how what is actually being rendered effects the whole processes.

--- Terapixel_2/src/GPU_Power_Usage_code.R
+++ Terapixel_2/src/GPU_Power_Usage_code.R
@@ -1,5 +1,46 @@
 
 gpu_render_highlight_sum <- gpu_render_highlight %>% group_by(render)  %>%  summarize(min = min(powerDrawWatt.x) , qt1 = quantile(powerDrawWatt.x, 1/4), mean = mean(powerDrawWatt.x), median =  median(powerDrawWatt.x), qt3 = quantile(powerDrawWatt.x, 3/4), max =max(powerDrawWatt.x), sd = sd(powerDrawWatt.x) )
 
+gpu_render_highlight_sum_jobId <- gpu_render_highlight %>% group_by(render,jobId)  %>%  summarize(min = min(powerDrawWatt.x) , qt1 = quantile(powerDrawWatt.x, 1/4), mean = mean(powerDrawWatt.x), median =  median(powerDrawWatt.x), qt3 = quantile(powerDrawWatt.x, 3/4), max =max(powerDrawWatt.x), sd = sd(powerDrawWatt.x) )
 
 gpu_render_highlight$render <- as.factor(gpu_render_highlight$render)
+
+Typ_power_V_render <- ggplot(gpu_render_highlight, aes(x = render, y = powerDrawWatt.x)) + geom_boxplot() + ggtitle("Typical Power usages rendering vs all other events")+ ylab("Power usage") 
+na.omit(gpu_render_highlight_sum_jobId)
+
+single_host <- gpu_render_highlight %>% filter(hostname == '04dc4e9647154250beeee51b866b0715000000')
+
+single_host_plot <- ggplot(single_host %>% filter(timestamp < as.POSIXct("2018-11-08 08:00:00")), aes(timestamp, powerDrawWatt.x)) + geom_line() + geom_point(aes(col = render))
+
+dat <- inner_join(gpu_render_highlight , gpu_render_highlight%>% group_by(taskId_event) %>% count(taskId_event), by = "taskId_event")
+
+
+
+with_percentage <- dat %>% group_by(taskId_event) %>% mutate(rendering_percenatge = (row_number() / n ) * 100)
+
+with_percentage
+
+with_percentage  %>% filter(render == 1) %>% group_by(render_time, rendering_percenatge) %>%  summarize(min = min(powerDrawWatt.x) , qt1 = quantile(powerDrawWatt.x, 1/4), mean = mean(powerDrawWatt.x), median =  median(powerDrawWatt.x), qt3 = quantile(powerDrawWatt.x, 3/4), max =max(powerDrawWatt.x), sd = sd(powerDrawWatt.x) )
+
+
+
+Power_used_at <- with_percentage  %>% filter(render == 1) %>% mutate(rendering_percenatge = round(rendering_percenatge,digits = 0)) %>% group_by(rendering_percenatge) %>% summarize(min = min(powerDrawWatt.x) , qt1 = quantile(powerDrawWatt.x, 1/4), mean = mean(powerDrawWatt.x), median =  median(powerDrawWatt.x), qt3 = quantile(powerDrawWatt.x, 3/4), max =max(powerDrawWatt.x), sd = sd(powerDrawWatt.x) )
+
+Power_used_at_percent <- with_percentage %>% mutate(render_time = round(render_time,digits = 0)) %>% filter(render == 1) %>% group_by(render_time, rendering_percenatge) %>%  summarize(min = min(powerDrawWatt.x) , qt1 = quantile(powerDrawWatt.x, 1/4), mean = mean(powerDrawWatt.x), median =  median(powerDrawWatt.x), qt3 = quantile(powerDrawWatt.x, 3/4), max =max(powerDrawWatt.x), sd = sd(powerDrawWatt.x) )
+gpu_render_highlight_table <- gpu_render_highlight %>% filter(render == 1)
+
+gpu_render_highlight_table$render_time <- round(gpu_render_highlight_table$render_time, digits = 0 )
+
+gpu_render_highlight_table <- gpu_render_highlight_table  %>% group_by(render_time)  %>%  summarize(min = min(powerDrawWatt.x) , qt1 = quantile(powerDrawWatt.x, 1/4), mean = mean(powerDrawWatt.x), median =  median(powerDrawWatt.x), qt3 = quantile(powerDrawWatt.x, 3/4), max =max(powerDrawWatt.x), sd = sd(powerDrawWatt.x))
+
+
+
+gpu_render_highlight_sum <- gpu_render_highlight %>% filter(render == 1)
+
+gpu_render_highlight_sum$render_time <- round(gpu_render_highlight_sum$render_time, digits = 0 )
+
+
+gpu_render_highlight_sum <- gpu_render_highlight_sum  %>% group_by(render_time,taskId_event)  %>%  summarize(sum = sum(powerDrawWatt.x))
+
+
+gpu_render_highlight_sum <-  gpu_render_highlight_sum  %>% group_by(render_time)  %>% summarize(min = min(sum) , qt1 = quantile(sum, 1/4), mean = mean(sum), median =  median(sum), qt3 = quantile(sum, 3/4), max =max(sum), sd = sd(sum))
